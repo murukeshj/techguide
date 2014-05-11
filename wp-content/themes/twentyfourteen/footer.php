2d3bec1d8eef80639c093c4de825abbee38dc352
murukeshj <murukeshj@gmail.com>
2014-05-11 23:10:04 +0530

    footer font size increased

--- wp-content/themes/twentyfourteen/footer.php
+++ wp-content/themes/twentyfourteen/footer.php
@@ -21,7 +21,7 @@
 				<a href="<?php echo esc_url( __( 'http://www.mumas.com/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Copyright &copy; 2014 %s', 'twentyfourteen' ), 'muma' ); ?></a>
                                 
 			</div><!-- .site-info -->
-                        <span style="margin-left:30px; font-size: 11px;">
+                        <span style="margin-left:30px; font-size: 12px;">
                                 All content is licensed under the <a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
                                     Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported </a> License. <br/>
                         </span>
